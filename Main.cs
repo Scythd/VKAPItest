f38276bd73c29e1176036b09e2ffbb346c177a03
никита мокляк <DezertirFeed@gmail.com>
2020-11-08 22:27:09 +0400

    Трай фиксации

---
+++ Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Main
+{
+    class Programm
+    {
+
+
+        public static void Main(string[] args)
+        {
+            Console.Out.WriteLine(VKAPI.VKAPI.VkAsk("https://api.vk.com/method/users.get?user_id=210700286&v=5.52"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VKAPI.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace VKAPI
+{
+    public class VKAPI
+    {
+
+
+        public static string VkAsk(string url) {
+            System.Net.WebRequest webRequest = System.Net.WebRequest.Create(url);
+            System.Net.WebResponse webResponse = (System.Net.WebResponse)webRequest.GetResponse();
+            System.IO.Stream dataStream = webResponse.GetResponseStream();
+            System.IO.StreamReader reader = new System.IO.StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            webResponse.Close();
+            return responseFromServer;
+        }
+    }
+
+    
+}
